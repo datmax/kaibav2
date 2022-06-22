c3ff0d694397bd995d2546250a44ed5623b874ef
datmax <max.datsun@student.univaq.it>
2022-06-22 17:28:32 +0200

    Miao

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,5 +1,13 @@
 export default function Footer() {
-  return <div className="text-white">
-      
-  </div>
+  return (
+    <div className="w-full text-white">
+      <div className="mx w-full">
+        <div className="mx-auto w-full text-center font-thin">
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSd0x_3_t_OVojoI7dHz2X5nU9cf9UMWmhhs0iw4rCQ32abAAA/viewform">
+            Feedback form
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
