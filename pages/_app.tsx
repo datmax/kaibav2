6c5c7519ca77ecf77d5edd9d8c796816d7f97a34
datmax <max.datsun@student.univaq.it>
2022-06-22 20:56:13 +0200

    Miao

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,41 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
 import { Web3Provider } from '../context/web3Context'
-
+import { useEffect } from 'react'
+import * as gtag from '../lib/gtag'
+import { useRouter } from 'next/router'
+import Script from 'next/script'
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
   return (
     <Web3Provider>
-      
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
